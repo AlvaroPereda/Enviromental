b8912576c427eb2ce6c06e6bcdd703093c5b9294
Álvaro Pereda Sánchez <121577598+AlvaroPereda@users.noreply.github.com>
2025-03-29 11:50:42 +0000

    update

--- utils/database.ts
+++ utils/database.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb"
 import { ItemsModel } from "./type.ts";
 
-const url = Deno.env.get("MONGO_URL")
+const url = Deno.env.get("MONGO")
 console.log("URL de MongoDB:", url); // Verifica la URL
 if(!url) throw new Error("con MONGO_URL")
 
